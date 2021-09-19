0b90d3cc7354388991e27b33e625cb6812130715
SupercoolU30 <71199764+SupercoolU30@users.noreply.github.com>
2021-09-19 10:51:22 -0700

    Create 3270518370.lua

---
+++ 3270518370.lua
@@ -0,0 +1,513 @@
+local ScreenGui = Instance.new("ScreenGui")
+local Main = Instance.new("Frame")
+local Selectframe = Instance.new("Frame")
+local ImageButton = Instance.new("ImageButton")
+local ImageButton_2 = Instance.new("ImageButton")
+local ImageButton_3 = Instance.new("ImageButton")
+local Frame = Instance.new("Frame")
+local ImageButton_4 = Instance.new("ImageButton")
+local TextLabel = Instance.new("TextLabel")
+local LOCALSCRIPTS = Instance.new("Frame")
+local NormalSpeed = Instance.new("TextButton")
+local UICorner = Instance.new("UICorner")
+local Normaljump = Instance.new("TextButton")
+local UICorner_2 = Instance.new("UICorner")
+local Sit = Instance.new("TextButton")
+local UICorner_3 = Instance.new("UICorner")
+local SuperJump = Instance.new("TextButton")
+local UICorner_4 = Instance.new("UICorner")
+local SuperSpeed = Instance.new("TextButton")
+local UICorner_5 = Instance.new("UICorner")
+local Tptool = Instance.new("TextButton")
+local UICorner_6 = Instance.new("UICorner")
+local Frame_2 = Instance.new("Frame")
+local SCRIPTS = Instance.new("Frame")
+local AnimationHub = Instance.new("TextButton")
+local UICorner_7 = Instance.new("UICorner")
+local Bypass = Instance.new("TextButton")
+local UICorner_8 = Instance.new("UICorner")
+local CMDx = Instance.new("TextButton")
+local UICorner_9 = Instance.new("UICorner")
+local DomainHub = Instance.new("TextButton")
+local UICorner_10 = Instance.new("UICorner")
+local Fatesadmin = Instance.new("TextButton")
+local UICorner_11 = Instance.new("UICorner")
+local Infiniteyield = Instance.new("TextButton")
+local UICorner_12 = Instance.new("UICorner")
+local Frame_3 = Instance.new("Frame")
+local CREDITS = Instance.new("Frame")
+local Pizzaman10820462 = Instance.new("TextLabel")
+local UICorner_13 = Instance.new("UICorner")
+local hacar1452 = Instance.new("TextLabel")
+local UICorner_14 = Instance.new("UICorner")
+local Frame_4 = Instance.new("Frame")
+local Home = Instance.new("Frame")
+local Frame_5 = Instance.new("Frame")
+local TextLabel_2 = Instance.new("TextLabel")
+local ImageLabel = Instance.new("ImageLabel")
+local Frame_6 = Instance.new("Frame")
+local TextLabel_3 = Instance.new("TextLabel")
+
+--Properties:
+
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Main.Name = "Main"
+Main.Parent = ScreenGui
+Main.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
+Main.BorderSizePixel = 0
+Main.Position = UDim2.new(0.175994486, 0, 0.203446984, 0)
+Main.Selectable = true
+Main.Size = UDim2.new(0, 517, 0, 279)
+Main.Active = true
+Main.Draggable = true
+
+Selectframe.Name = "Select frame"
+Selectframe.Parent = Main
+Selectframe.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Selectframe.BorderColor3 = Color3.fromRGB(35, 35, 35)
+Selectframe.BorderSizePixel = 0
+Selectframe.Position = UDim2.new(0.00193423603, 0, 0.0931899622, 0)
+Selectframe.Size = UDim2.new(0, 49, 0, 253)
+
+ImageButton.Parent = Selectframe
+ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageButton.BackgroundTransparency = 2.000
+ImageButton.Position = UDim2.new(0.117969237, 0, 0.482286483, 0)
+ImageButton.Size = UDim2.new(0, 32, 0, 31)
+ImageButton.Image = "rbxassetid://3019710362"
+
+ImageButton_2.Parent = Selectframe
+ImageButton_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageButton_2.BackgroundTransparency = 2.000
+ImageButton_2.Position = UDim2.new(0.158785447, 0, 0.833729625, 0)
+ImageButton_2.Size = UDim2.new(0, 33, 0, 32)
+ImageButton_2.Image = "rbxassetid://422958177"
+
+ImageButton_3.Parent = Selectframe
+ImageButton_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageButton_3.BackgroundTransparency = 2.000
+ImageButton_3.Position = UDim2.new(0.117969237, 0, 0.222146764, 0)
+ImageButton_3.Size = UDim2.new(0, 32, 0, 31)
+ImageButton_3.Image = "http://www.roblox.com/asset/?id=2795572800"
+
+Frame.Parent = Main
+Frame.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Frame.BorderColor3 = Color3.fromRGB(35, 35, 35)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0, 0, -0.0215053763, 0)
+Frame.Size = UDim2.new(0, 517, 0, 33)
+
+ImageButton_4.Parent = Frame
+ImageButton_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageButton_4.BackgroundTransparency = 2.000
+ImageButton_4.Position = UDim2.new(0.011269887, 0, -0.00782012939, 0)
+ImageButton_4.Size = UDim2.new(0, 38, 0, 38)
+ImageButton_4.Image = "http://www.roblox.com/asset/?id=7338012456"
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 2.000
+TextLabel.Position = UDim2.new(0.411992192, 0, -0.0303030312, 0)
+TextLabel.Size = UDim2.new(0, 132, 0, 33)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "Bacon Hub"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
+
+LOCALSCRIPTS.Name = "LOCALSCRIPTS"
+LOCALSCRIPTS.Parent = Main
+LOCALSCRIPTS.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
+LOCALSCRIPTS.BorderColor3 = Color3.fromRGB(35, 35, 35)
+LOCALSCRIPTS.Position = UDim2.new(0.0947775617, 0, 0.0788530484, 0)
+LOCALSCRIPTS.Size = UDim2.new(0, 467, 0, 257)
+LOCALSCRIPTS.Visible = false
+LOCALSCRIPTS.ZIndex = 2
+
+NormalSpeed.Name = "Normal Speed"
+NormalSpeed.Parent = LOCALSCRIPTS
+NormalSpeed.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+NormalSpeed.BorderColor3 = Color3.fromRGB(35, 35, 35)
+NormalSpeed.Position = UDim2.new(0.520309508, 0, 0.372759789, 0)
+NormalSpeed.Size = UDim2.new(0, 194, 0, 42)
+NormalSpeed.Font = Enum.Font.SourceSans
+NormalSpeed.Text = "Normal Speed"
+NormalSpeed.TextColor3 = Color3.fromRGB(255, 255, 255)
+NormalSpeed.TextSize = 36.000
+NormalSpeed.MouseButton1Down:connect(function()
+	local plr = game.Players.LocalPlayer
+	local char = plr.Character
+	char.Humanoid.WalkSpeed = 16
+end)
+
+UICorner.Parent = NormalSpeed
+
+Normaljump.Name = "Normal jump"
+Normaljump.Parent = LOCALSCRIPTS
+Normaljump.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Normaljump.BorderColor3 = Color3.fromRGB(35, 35, 35)
+Normaljump.Position = UDim2.new(0.520309508, 0, 0.0967741311, 0)
+Normaljump.Size = UDim2.new(0, 194, 0, 42)
+Normaljump.Font = Enum.Font.SourceSans
+Normaljump.Text = "Normal Jump"
+Normaljump.TextColor3 = Color3.fromRGB(255, 255, 255)
+Normaljump.TextSize = 36.000
+Normaljump.MouseButton1Down:connect(function()
+	local plr = game.Players.LocalPlayer
+	local char = plr.Character
+	char.Humanoid.JumpPower = 50
+end)
+
+UICorner_2.Parent = Normaljump
+
+Sit.Name = "Sit"
+Sit.Parent = LOCALSCRIPTS
+Sit.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Sit.BorderColor3 = Color3.fromRGB(35, 35, 35)
+Sit.Position = UDim2.new(0.520309508, 0, 0.659498155, 0)
+Sit.Size = UDim2.new(0, 194, 0, 42)
+Sit.Font = Enum.Font.SourceSans
+Sit.Text = "Sit"
+Sit.TextColor3 = Color3.fromRGB(255, 255, 255)
+Sit.TextSize = 36.000
+Sit.MouseButton1Down:connect(function()
+	local plr = game.Players.LocalPlayer
+	local char = plr.Character
+	char.Humanoid.Sit = true
+end)
+
+UICorner_3.Parent = Sit
+
+SuperJump.Name = "Super Jump"
+SuperJump.Parent = LOCALSCRIPTS
+SuperJump.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+SuperJump.BorderColor3 = Color3.fromRGB(35, 35, 35)
+SuperJump.Position = UDim2.new(0.0715667605, 0, 0.0967741907, 0)
+SuperJump.Size = UDim2.new(0, 194, 0, 42)
+SuperJump.Font = Enum.Font.SourceSans
+SuperJump.Text = "Super Jump"
+SuperJump.TextColor3 = Color3.fromRGB(255, 255, 255)
+SuperJump.TextSize = 36.000
+SuperJump.MouseButton1Down:connect(function()
+	local plr = game.Players.LocalPlayer
+	local char = plr.Character
+	char.Humanoid.JumpPower = 100
+end)
+
+UICorner_4.Parent = SuperJump
+
+SuperSpeed.Name = "Super Speed"
+SuperSpeed.Parent = LOCALSCRIPTS
+SuperSpeed.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+SuperSpeed.BorderColor3 = Color3.fromRGB(35, 35, 35)
+SuperSpeed.Position = UDim2.new(0.0715667605, 0, 0.369175613, 0)
+SuperSpeed.Size = UDim2.new(0, 194, 0, 42)
+SuperSpeed.Font = Enum.Font.SourceSans
+SuperSpeed.Text = "Super Speed"
+SuperSpeed.TextColor3 = Color3.fromRGB(255, 255, 255)
+SuperSpeed.TextSize = 36.000
+SuperSpeed.MouseButton1Down:connect(function()
+	local plr = game.Players.LocalPlayer
+	local char = plr.Character
+	char.Humanoid.WalkSpeed = 50
+end)
+
+UICorner_5.Parent = SuperSpeed
+
+Tptool.Name = "Tp tool"
+Tptool.Parent = LOCALSCRIPTS
+Tptool.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Tptool.BorderColor3 = Color3.fromRGB(35, 35, 35)
+Tptool.Position = UDim2.new(0.0715667307, 0, 0.659498155, 0)
+Tptool.Size = UDim2.new(0, 194, 0, 42)
+Tptool.Font = Enum.Font.SourceSans
+Tptool.Text = "TP tool"
+Tptool.TextColor3 = Color3.fromRGB(255, 255, 255)
+Tptool.TextSize = 36.000
+Tptool.MouseButton1Down:connect(function()
+	mouse = game.Players.LocalPlayer:GetMouse()
+	tool = Instance.new("Tool")
+	tool.RequiresHandle = false
+	tool.Name = "Click Teleport"
+	tool.Activated:connect(function()
+		local pos = mouse.Hit+Vector3.new(0,2.5,0)
+		pos = CFrame.new(pos.X,pos.Y,pos.Z)
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pos
+	end)
+	tool.Parent = game.Players.LocalPlayer.Backpack
+end)
+
+UICorner_6.Parent = Tptool
+
+Frame_2.Parent = LOCALSCRIPTS
+Frame_2.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Frame_2.BorderSizePixel = 0
+Frame_2.Position = UDim2.new(-0.0641026273, 0, 0.963990569, 0)
+Frame_2.Size = UDim2.new(0, 498, 0, 8)
+
+SCRIPTS.Name = "SCRIPTS"
+SCRIPTS.Parent = Main
+SCRIPTS.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
+SCRIPTS.BorderColor3 = Color3.fromRGB(35, 35, 35)
+SCRIPTS.Position = UDim2.new(0.0967117995, 0, 0.0788530484, 0)
+SCRIPTS.Size = UDim2.new(0, 467, 0, 257)
+SCRIPTS.Visible = false
+SCRIPTS.ZIndex = 2
+
+AnimationHub.Name = "Animation Hub"
+AnimationHub.Parent = SCRIPTS
+AnimationHub.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+AnimationHub.BorderColor3 = Color3.fromRGB(35, 35, 35)
+AnimationHub.Position = UDim2.new(0.524177969, 0, 0.365591377, 0)
+AnimationHub.Size = UDim2.new(0, 194, 0, 42)
+AnimationHub.Font = Enum.Font.SourceSans
+AnimationHub.Text = "Animation Hub"
+AnimationHub.TextColor3 = Color3.fromRGB(255, 255, 255)
+AnimationHub.TextSize = 36.000
+AnimationHub.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://gist.githubusercontent.com/M6HqVBcddw2qaN4s/37eef2120d509b37b31fa73944ab2361/raw/kT2fVEFnzDfCRXAP"))()
+end)
+
+UICorner_7.Parent = AnimationHub
+
+Bypass.Name = "Bypass"
+Bypass.Parent = SCRIPTS
+Bypass.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Bypass.BorderColor3 = Color3.fromRGB(35, 35, 35)
+Bypass.Position = UDim2.new(0.524177969, 0, 0.111111119, 0)
+Bypass.Size = UDim2.new(0, 194, 0, 42)
+Bypass.Font = Enum.Font.SourceSans
+Bypass.Text = "ChatByPass"
+Bypass.TextColor3 = Color3.fromRGB(255, 255, 255)
+Bypass.TextSize = 36.000
+Bypass.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://the-shed.xyz/roblox/scripts/ChatBypass", true))()
+end)
+
+UICorner_8.Parent = Bypass
+
+CMDx.Name = "CMD-x"
+CMDx.Parent = SCRIPTS
+CMDx.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+CMDx.BorderColor3 = Color3.fromRGB(35, 35, 35)
+CMDx.Position = UDim2.new(0.0657640249, 0, 0.637992859, 0)
+CMDx.Size = UDim2.new(0, 194, 0, 42)
+CMDx.Font = Enum.Font.SourceSans
+CMDx.Text = "CMD-X"
+CMDx.TextColor3 = Color3.fromRGB(255, 255, 255)
+CMDx.TextSize = 36.000
+CMDx.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://pastebin.com/raw/HFkyyd1H",true))()
+end)
+
+UICorner_9.Parent = CMDx
+
+DomainHub.Name = "Domain Hub"
+DomainHub.Parent = SCRIPTS
+DomainHub.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+DomainHub.BorderColor3 = Color3.fromRGB(35, 35, 35)
+DomainHub.Position = UDim2.new(0.524177969, 0, 0.637992859, 0)
+DomainHub.Size = UDim2.new(0, 194, 0, 42)
+DomainHub.Font = Enum.Font.SourceSans
+DomainHub.Text = "Domain Hub"
+DomainHub.TextColor3 = Color3.fromRGB(255, 255, 255)
+DomainHub.TextSize = 36.000
+DomainHub.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexsoftworks/Domain/main/source'),true))()
+end)
+
+UICorner_10.Parent = DomainHub
+
+Fatesadmin.Name = "Fates admin"
+Fatesadmin.Parent = SCRIPTS
+Fatesadmin.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Fatesadmin.BorderColor3 = Color3.fromRGB(35, 35, 35)
+Fatesadmin.Position = UDim2.new(0.0657640249, 0, 0.365591377, 0)
+Fatesadmin.Size = UDim2.new(0, 194, 0, 42)
+Fatesadmin.Font = Enum.Font.SourceSans
+Fatesadmin.Text = "Fates admin"
+Fatesadmin.TextColor3 = Color3.fromRGB(255, 255, 255)
+Fatesadmin.TextSize = 36.000
+Fatesadmin.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/fatesc/fates-admin/main/main.lua"))();
+end)
+
+UICorner_11.Parent = Fatesadmin
+
+Infiniteyield.Name = "Infinite yield"
+Infiniteyield.Parent = SCRIPTS
+Infiniteyield.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Infiniteyield.BorderColor3 = Color3.fromRGB(35, 35, 35)
+Infiniteyield.Position = UDim2.new(0.0657640249, 0, 0.11111109, 0)
+Infiniteyield.Size = UDim2.new(0, 194, 0, 42)
+Infiniteyield.Font = Enum.Font.SourceSans
+Infiniteyield.Text = "Infinite yield"
+Infiniteyield.TextColor3 = Color3.fromRGB(255, 255, 255)
+Infiniteyield.TextSize = 36.000
+Infiniteyield.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+end)
+
+UICorner_12.Parent = Infiniteyield
+
+Frame_3.Parent = SCRIPTS
+Frame_3.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Frame_3.BorderSizePixel = 0
+Frame_3.Position = UDim2.new(-0.0641026273, 0, 0.963990569, 0)
+Frame_3.Size = UDim2.new(0, 498, 0, 8)
+
+CREDITS.Name = "CREDITS"
+CREDITS.Parent = Main
+CREDITS.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
+CREDITS.BorderColor3 = Color3.fromRGB(35, 35, 35)
+CREDITS.Position = UDim2.new(0.0947775617, 0, 0.0788530484, 0)
+CREDITS.Size = UDim2.new(0, 467, 0, 257)
+CREDITS.Visible = false
+CREDITS.ZIndex = 2
+
+Pizzaman10820462.Name = "Pizzaman1082#0462"
+Pizzaman10820462.Parent = CREDITS
+Pizzaman10820462.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Pizzaman10820462.Position = UDim2.new(0.508704066, 0, 0.322580695, 0)
+Pizzaman10820462.Size = UDim2.new(0, 200, 0, 50)
+Pizzaman10820462.Font = Enum.Font.SourceSans
+Pizzaman10820462.Text = "Pizzaman1082#0462"
+Pizzaman10820462.TextColor3 = Color3.fromRGB(255, 255, 255)
+Pizzaman10820462.TextSize = 28.000
+
+UICorner_13.Parent = Pizzaman10820462
+
+hacar1452.Name = "hacar#1452"
+hacar1452.Parent = CREDITS
+hacar1452.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+hacar1452.Position = UDim2.new(0.0715667233, 0, 0.322580695, 0)
+hacar1452.Size = UDim2.new(0, 200, 0, 50)
+hacar1452.Font = Enum.Font.SourceSans
+hacar1452.Text = "hacar#1452"
+hacar1452.TextColor3 = Color3.fromRGB(255, 255, 255)
+hacar1452.TextSize = 46.000
+
+UICorner_14.Parent = hacar1452
+
+Frame_4.Parent = CREDITS
+Frame_4.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Frame_4.BorderSizePixel = 0
+Frame_4.Position = UDim2.new(-0.0641026273, 0, 0.963990569, 0)
+Frame_4.Size = UDim2.new(0, 498, 0, 8)
+
+Home.Name = "Home"
+Home.Parent = Main
+Home.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
+Home.BorderColor3 = Color3.fromRGB(35, 35, 35)
+Home.BorderSizePixel = 0
+Home.Position = UDim2.new(0.0947775617, 0, 0.111111112, 0)
+Home.Size = UDim2.new(0, 468, 0, 248)
+
+Frame_5.Parent = Home
+Frame_5.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Frame_5.BorderSizePixel = 0
+Frame_5.Position = UDim2.new(0.192307696, 0, 0.528506637, 0)
+Frame_5.Size = UDim2.new(0, 238, 0, 81)
+
+TextLabel_2.Parent = Frame_5
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BackgroundTransparency = 2.000
+TextLabel_2.Position = UDim2.new(0.246230543, 0, 0.197530866, 0)
+TextLabel_2.Size = UDim2.new(0, 179, 0, 49)
+TextLabel_2.Font = Enum.Font.SourceSans
+TextLabel_2.Text = "Running Bacon Hub v1.0 Stable"
+TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.TextScaled = true
+TextLabel_2.TextSize = 14.000
+TextLabel_2.TextWrapped = true
+
+ImageLabel.Parent = Frame_5
+ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel.BackgroundTransparency = 2.000
+ImageLabel.Position = UDim2.new(0.0294117648, 0, 0.172839507, 0)
+ImageLabel.Size = UDim2.new(0, 51, 0, 53)
+ImageLabel.Image = "rbxassetid://3019429456"
+
+Frame_6.Parent = Home
+Frame_6.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+Frame_6.BorderSizePixel = 0
+Frame_6.Position = UDim2.new(-0.0641026273, 0, 0.963990569, 0)
+Frame_6.Size = UDim2.new(0, 498, 0, 8)
+
+TextLabel_3.Parent = Home
+TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.BackgroundTransparency = 2.000
+TextLabel_3.Position = UDim2.new(0.100427411, 0, 0.0322580636, 0)
+TextLabel_3.Size = UDim2.new(0, 374, 0, 67)
+TextLabel_3.Font = Enum.Font.SourceSans
+TextLabel_3.Text = "Hello, Thanks for choosing bacon hub!"
+TextLabel_3.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.TextScaled = true
+TextLabel_3.TextSize = 14.000
+TextLabel_3.TextWrapped = true
+
+-- Scripts:
+
+local function TAHNYQ_fake_script() -- ImageButton.LocalScript 
+	local script = Instance.new('LocalScript', ImageButton)
+
+	script.parent.MouseButton1Down:connect(function()
+		script.Parent.Parent.Parent.LOCALSCRIPTS.Visible = false
+		script.Parent.Parent.Parent.SCRIPTS.Visible = true
+		script.Parent.Parent.Parent.CREDITS.Visible = false
+		script.Parent.Parent.Parent.Home.Visible = false
+	
+	end)
+	
+end
+coroutine.wrap(TAHNYQ_fake_script)()
+local function QIXPRK_fake_script() -- ImageButton_2.LocalScript 
+	local script = Instance.new('LocalScript', ImageButton_2)
+
+	script.parent.MouseButton1Down:connect(function()
+		script.Parent.Parent.Parent.LOCALSCRIPTS.Visible = false
+		script.Parent.Parent.Parent.SCRIPTS.Visible = false
+		script.Parent.Parent.Parent.CREDITS.Visible = true
+		script.Parent.Parent.Parent.Home.Visible = false
+	
+	end)
+	
+end
+coroutine.wrap(QIXPRK_fake_script)()
+local function ZADLIJQ_fake_script() -- ImageButton_3.LocalScript 
+	local script = Instance.new('LocalScript', ImageButton_3)
+
+	script.parent.MouseButton1Down:connect(function()
+		script.Parent.Parent.Parent.LOCALSCRIPTS.Visible = true
+		script.Parent.Parent.Parent.SCRIPTS.Visible = false
+		script.Parent.Parent.Parent.CREDITS.Visible = false
+		script.Parent.Parent.Parent.Home.Visible = false
+	
+		end)
+	
+end
+coroutine.wrap(ZADLIJQ_fake_script)()
+local function CZCAYM_fake_script() -- ImageButton_4.LocalScript 
+	local script = Instance.new('LocalScript', ImageButton_4)
+
+	script.parent.MouseButton1Down:connect(function()
+		script.Parent.Parent.Parent.LOCALSCRIPTS.Visible = false
+		script.Parent.Parent.Parent.SCRIPTS.Visible = false
+		script.Parent.Parent.Parent.CREDITS.Visible = false
+		script.Parent.Parent.Parent.Home.Visible = true
+	end)
+	
+end
+coroutine.wrap(CZCAYM_fake_script)()
+
+local notifSound = Instance.new("Sound",workspace)
+notifSound.PlaybackSpeed = 1.0
+notifSound.Volume = 0.7
+notifSound.SoundId = "rbxassetid://1052934249"
+notifSound.PlayOnRemove = true
+notifSound:Destroy()
+game.StarterGui:SetCore("SendNotification", {Title = "BaconHub", Text = "Thanks for using this GUI", Icon = "rbxassetid://7338012456", Duration = 5})
+")()")()
